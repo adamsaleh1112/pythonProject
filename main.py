07cf66d798b93b9a92b88eff57b0ac09deeb6eac
adamsaleh1112 <144299219+adamsaleh1112@users.noreply.github.com>
2024-01-18 18:30:51 -0500

    Commit and push

--- main.py
+++ main.py
@@ -8,10 +8,13 @@ from login import loginscreen # Importing login screen function from login.py
 from createaccount import * # Importing everything from createaccount.py
 from gui import * # Importing everything from gui.py
 import os # Importing file managing library, which allows for editing and reading other files
+from logdefiner import get_log_name
 
-
-if not os.path.exists("log.txt"): # Checks if log.txt doesnt exist
-    open("log.txt", "w").close() # If not, then it creates one
+filename = get_log_name()
+if os.path.exists(filename):
+   pass
+else:
+   f = open(filename, "w")
 
 
 #Builds Login Screen - has create account and make gui inside of it
--- movement.py
+++ movement.py
@@ -1,93 +1,95 @@
 import requests # Importing requests, a library that allows commands to be sent to ip's
 from datetime import datetime # Importing a dat time library, allows date and time to be accessed
+from logdefiner import get_log_name
 
+filename = get_log_name()
 
 def forward(): # Forward command
-    url = 'http://192.168.1.21:5000/forward' # Sending command to ip route forward
-    data = {'command': 'forward'} # Sending forward command
-    response = requests.post(url, data=data) # Making the data being sent = forward
+   url = 'http://192.168.1.34:5000/forward' # Sending command to ip route forward
+   data = {'command': 'forward'} # Sending forward command
+   response = requests.post(url, data=data) # Making the data being sent = forward
 
-    with open('usernamelog.txt', 'r') as f: # Opening usernamelog.txt to read
-        username = str(f.readlines()[-1:]) # Reading it
-        username = username.strip("['") # Removing unwanted characters
-        username = username.strip("']")
+   with open('usernamelog.txt', 'r') as f: # Opening usernamelog.txt to read
+       username = str(f.readlines()[-1:]) # Reading it
+       username = username.strip("['") # Removing unwanted characters
+       username = username.strip("']")
 
-    with open('log.txt', 'a') as f: # Opening log.txt to append
-        now = datetime.now() # Getting date and time
-        dt_string = now.strftime("%d/%m/%Y %H:%M:%S") # Setting date and time string
-        f.write(username + ": " + dt_string + ' Move forward \n') # Combing strings and sending it to log.txt
+   with open(filename, 'a') as f: # Opening log.txt to append
+       now = datetime.now() # Getting date and time
+       dt_string = now.strftime("%d/%m/%Y %H:%M:%S") # Setting date and time string
+       f.write(username + ": " + dt_string + ' Move forward \n') # Combing strings and sending it to log.txt
 
 def backward(): # Same as forward command, but command being sent is 'backward'
-    url = 'http://192.168.1.21:5000/backward'
-    data = {'command': 'backward'}
-    response = requests.post(url, data=data)
+   url = 'http://192.168.1.34:5000/backward'
+   data = {'command': 'backward'}
+   response = requests.post(url, data=data)
 
-    with open('usernamelog.txt', 'r') as f:
-        username = str(f.readlines()[-1:])
-        username = username.strip("['")
-        username = username.strip("']")
+   with open('usernamelog.txt', 'r') as f:
+       username = str(f.readlines()[-1:])
+       username = username.strip("['")
+       username = username.strip("']")
 
-    with open('log.txt', 'a') as f:
-        now = datetime.now()
-        dt_string = now.strftime("%d/%m/%Y %H:%M:%S")
-        f.write(username + ": " + dt_string + ' Move backward \n')
+   with open(filename, 'a') as f:
+       now = datetime.now()
+       dt_string = now.strftime("%d/%m/%Y %H:%M:%S")
+       f.write(username + ": " + dt_string + ' Move backward \n')
 
 def left(): # Same as forward command, but command being sent is 'left'
-    url = 'http://192.168.1.21:5000/left'
-    data = {'command': 'left'}
-    response = requests.post(url, data=data)
+   url = 'http://192.168.1.34:5000/left'
+   data = {'command': 'left'}
+   response = requests.post(url, data=data)
 
-    with open('usernamelog.txt', 'r') as f:
-        username = str(f.readlines()[-1:])
-        username = username.strip("['")
-        username = username.strip("']")
+   with open('usernamelog.txt', 'r') as f:
+       username = str(f.readlines()[-1:])
+       username = username.strip("['")
+       username = username.strip("']")
 
-    with open('log.txt', 'a') as f:
-        now = datetime.now()
-        dt_string = now.strftime("%d/%m/%Y %H:%M:%S")
-        f.write(username + ": " + dt_string + ' Move left \n')
+   with open(filename, 'a') as f:
+       now = datetime.now()
+       dt_string = now.strftime("%d/%m/%Y %H:%M:%S")
+       f.write(username + ": " + dt_string + ' Move left \n')
 
 def right(): # Same as forward command, but command being sent is 'right'
-    url = 'http://192.168.1.21:5000/right'
-    data = {'command': 'right'}
-    response = requests.post(url, data=data)
+   url = 'http://192.168.1.34:5000/right'
+   data = {'command': 'right'}
+   response = requests.post(url, data=data)
 
-    with open('usernamelog.txt', 'r') as f:
-        username = str(f.readlines()[-1:])
-        username = username.strip("['")
-        username = username.strip("']")
+   with open('usernamelog.txt', 'r') as f:
+       username = str(f.readlines()[-1:])
+       username = username.strip("['")
+       username = username.strip("']")
 
-    with open('log.txt', 'a') as f:
-        now = datetime.now()
-        dt_string = now.strftime("%d/%m/%Y %H:%M:%S")
-        f.write(username + ": " + dt_string + ' Move right \n')
+   with open(filename, 'a') as f:
+       now = datetime.now()
+       dt_string = now.strftime("%d/%m/%Y %H:%M:%S")
+       f.write(username + ": " + dt_string + ' Move right \n')
 
 def stop(): # Same as forward command, but command being sent is 'stop'
-    url = 'http://192.168.1.21:5000/stop'
-    data = {'command': 'stop'}
-    response = requests.post(url, data=data)
+   url = 'http://192.168.1.34:5000/stop'
+   data = {'command': 'stop'}
+   response = requests.post(url, data=data)
 
-    with open('usernamelog.txt', 'r') as f:
-        username = str(f.readlines()[-1:])
-        username = username.strip("['")
-        username = username.strip("']")
+   with open('usernamelog.txt', 'r') as f:
+       username = str(f.readlines()[-1:])
+       username = username.strip("['")
+       username = username.strip("']")
 
-    with open('log.txt', 'a') as f:
-        now = datetime.now()
-        dt_string = now.strftime("%d/%m/%Y %H:%M:%S")
-        f.write(username + ": " + dt_string + ' Stop \n')
+   with open(filename, 'a') as f:
+       now = datetime.now()
+       dt_string = now.strftime("%d/%m/%Y %H:%M:%S")
+       f.write(username + ": " + dt_string + ' Stop \n')
 
 def go(): # Same as forward command, but command being sent is 'go'
-    url = 'http://192.168.1.21:5000/go'
-    data = {'command': 'go'}
-    response = requests.post(url, data=data)
+   url = 'http://192.168.1.34:5000/go'
+   data = {'command': 'go'}
+   response = requests.post(url, data=data)
 
-    with open('usernamelog.txt', 'r') as f:
-        username = str(f.readlines()[-1:])
-        username = username.strip("['")
-        username = username.strip("']")
+   with open('usernamelog.txt', 'r') as f:
+       username = str(f.readlines()[-1:])
+       username = username.strip("['")
+       username = username.strip("']")
 
-    with open('log.txt', 'a') as f:
-        now = datetime.now()
-        dt_string = now.strftime("%d/%m/%Y %H:%M:%S")
-        f.write(username + ": " + dt_string + ' Go \n')
+   with open(filename, 'a') as f:
+       now = datetime.now()
+       dt_string = now.strftime("%d/%m/%Y %H:%M:%S")
+       f.write(username + ": " + dt_string + ' Go \n')
